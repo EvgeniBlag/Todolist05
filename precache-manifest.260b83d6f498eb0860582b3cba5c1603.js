852792f6b9de729393ccdca4e73b374e03797ad4
EvgeniBlag <blagodarni@gmail.com>
2022-06-07 10:05:29 +0200

    Updates

---
+++ precache-manifest.260b83d6f498eb0860582b3cba5c1603.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d0feda67678a4981b0f59c2c86aebc",
+    "url": "/Todolist05/index.html"
+  },
+  {
+    "revision": "69657cc496ccaeb68d72",
+    "url": "/Todolist05/static/css/main.7ded4491.chunk.css"
+  },
+  {
+    "revision": "83e13e3da8a74328c3fa",
+    "url": "/Todolist05/static/js/2.2d81194d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todolist05/static/js/2.2d81194d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69657cc496ccaeb68d72",
+    "url": "/Todolist05/static/js/main.840b51f3.chunk.js"
+  },
+  {
+    "revision": "163701e68c02b8b3f39c",
+    "url": "/Todolist05/static/js/runtime-main.75d8b264.js"
+  }
+]);
